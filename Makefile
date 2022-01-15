d931e282f042fbbd2e91dcc1f1eba5268d48e8c0
Ayush Rathore <aayushh79@gmail.com>
2022-01-15 21:29:39 +0530

    Makefile for swayland installation

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-update-config:
+update:
 	cd update && ./update-config.sh
--- update/update-config.sh
+++
@@ -1,6 +0,0 @@
-cp -rv ${HOME}/.config/sway/ ../
-cp -rv ${HOME}/.config/waybar/ ../
-cp -rv ${HOME}/.config/alacritty/ ../
-cp -rv ${HOME}/.config/swaylock/ ../
-cp -rv ${HOME}/.config/wofi/ ../
-cp -rv ${HOME}/.config/mako/ ../
