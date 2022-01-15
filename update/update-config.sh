02b1ad9515b2b20a23824d496a45ede7cb414d46
Ayush Rathore <aayushh79@gmail.com>
2022-01-15 17:16:48 +0530

    Add script to update configs

---
+++ update/update-config.sh
@@ -0,0 +1,6 @@
+cp -rv ${HOME}/.config/sway/ ../
+cp -rv ${HOME}/.config/waybar/ ../
+cp -rv ${HOME}/.config/alacritty/ ../
+cp -rv ${HOME}/.config/swaylock/ ../
+cp -rv ${HOME}/.config/wofi/ ../
+cp -rv ${HOME}/.config/mako/ ../
